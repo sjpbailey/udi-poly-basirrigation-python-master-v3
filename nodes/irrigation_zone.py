160cf872efba955dead1e6b1effacd862ad7c9b9
sjpbailey <sjpbailey1961@gmail.com>
2022-07-13 23:39:02 -0700

    Fix universalInputs and README

--- nodes/irrigation_zone.py
+++ nodes/irrigation_zone.py
@@ -12,16 +12,17 @@ from bascontrolns import Device, Platform
 
 LOGGER = udi_interface.LOGGER
 
+
 class IrrigationNode(udi_interface.Node):
     def __init__(self, polyglot, primary, address, name, ip, ip1, ip2, ip3, ip4, ip5, bc):
         super(IrrigationNode, self).__init__(polyglot, primary, address, name)
         self.poly = polyglot
-        self.lpfx = '%s:%s' % (address,name)
+        self.lpfx = '%s:%s' % (address, name)
         self.poly.subscribe(self.poly.START, self.start, address)
         self.poly.subscribe(self.poly.POLL, self.poll)
         self.bc = bc
         LOGGER.info(address)
-        ### IP Address Sorter
+        # IP Address Sorter
         if address == 'zone_{}'.format(0):
             self.ipaddress = ip
         elif address == 'zone_{}'.format(1):
@@ -33,38 +34,41 @@ class IrrigationNode(udi_interface.Node):
         elif address == 'zone_{}'.format(4):
             self.ipaddress = ip4
         elif address == 'zone_{}'.format(5):
-            self.ipaddress = ip5            
+            self.ipaddress = ip5
         else:
-            pass    
+            pass
 
     def start(self):
         if self.ipaddress is not None:
             self.bc = Device(self.ipaddress)
-            ### Do we have a BASpi or an Edge Device ###
-            if self.bc.ePlatform == Platform.BASC_PI or self.bc.ePlatform == Platform.BASC_PO: ### if a BASpi-6u6r Device is found
-                LOGGER.info('connected to BASpi-6U6R')
-            elif self.bc.ePlatform == Platform.BASC_ED: ### if a BASpi-Edge Device is found
-                LOGGER.info('connected to BASpi-Edge')
-                self.setDriver('ST', 1)    
-            elif self.bc.ePlatform == Platform.BASC_NONE: ### if there is NO Device found
-                LOGGER.info('Unable to connect to Device')
-                LOGGER.info('ipaddress')
-            else:
-                pass
-        
-            # How many nodes or points does the device have
-            LOGGER.info('\t' + str(self.bc.uiQty) + ' Universal inputs in this Device')
-            LOGGER.info('\t' + str(self.bc.boQty) + ' Binary outputs in this Device')
-            LOGGER.info('\t' + str(self.bc.vtQty) + ' Virtual points In This Device')        
-        
-            # Input/Output Status
-            LOGGER.info('Inputs')
-            for i in range(1,7):
-                LOGGER.info(str(self.bc.universalInput(i)))
-            LOGGER.info('Outputs')
-            for i in range(1,7):
-                LOGGER.info(str(self.bc.binaryOutput(i)))
-        
+        ### Do we have a BASpi or an Edge Device ###
+        if self.bc.ePlatform == Platform.BASC_PI or self.bc.ePlatform == Platform.BASC_PO:  # if a BASpi-6u6r Device is found
+            LOGGER.info('connected to BASpi-6U6R')
+        elif self.bc.ePlatform == Platform.BASC_ED:  # if a BASpi-Edge Device is found
+            LOGGER.info('connected to BASpi-Edge')
+            self.setDriver('ST', 1)
+        elif self.bc.ePlatform == Platform.BASC_NONE:  # if there is NO Device found
+            LOGGER.info('Unable to connect to Device')
+            LOGGER.info('ipaddress')
+        else:
+            pass
+
+        # How many nodes or points does the device have
+        LOGGER.info('\t' + str(self.bc.uiQty) +
+                    ' Universal inputs in this Device')
+        LOGGER.info('\t' + str(self.bc.boQty) +
+                    ' Binary outputs in this Device')
+        LOGGER.info('\t' + str(self.bc.vtQty) +
+                    ' Virtual points In This Device')
+
+        # Input/Output Status
+        LOGGER.info('Inputs')
+        for i in range(1, 7):
+            LOGGER.info(str(self.bc.universalInput(i)))
+        LOGGER.info('Outputs')
+        for i in range(1, 7):
+            LOGGER.info(str(self.bc.binaryOutput(i)))
+
         ### Universal Inputs ###
         self.setInputDriver('GV0', 1)
         self.setInputDriver('GV1', 2)
@@ -78,7 +82,7 @@ class IrrigationNode(udi_interface.Node):
             sumss_count = int(float(input_six))
             self.setDriver('GV5', int(sumss_count))
         if input_six is not None:
-            sumss_count = int(float(self.bc.universalInput(6))) 
+            sumss_count = int(float(self.bc.universalInput(6)))
         else:
             return
 
@@ -100,7 +104,7 @@ class IrrigationNode(udi_interface.Node):
         self.setOutputDriver('GV10', 5)
         self.setOutputDriver('GV11', 6)
 
-    ### Binary Output Conversion ###    
+    ### Binary Output Conversion ###
     def setOutputDriver(self, driver, input):
         output_val = self.bc.binaryOutput(input)
         count = 0
@@ -108,25 +112,25 @@ class IrrigationNode(udi_interface.Node):
             count = (output_val)
             self.setDriver(driver, count)
         else:
-            return       
+            return
         pass
 
-        # Dict for 6 output ON OFF function     
+        # Dict for 6 output ON OFF function
         self.mapping = {
-            'BON1' : {'output':'GV6', 'index': (1)}, 
-            'BOF1' : {'output':'GV6', 'index': (1)}, 
-            'BON2' : {'output':'GV7', 'index': (2)}, 
-            'BOF2' : {'output':'GV7', 'index': (2)}, 
-            'BON3' : {'output':'GV8', 'index': (3)}, 
-            'BOF3' : {'output':'GV8', 'index': (3)},
-            'BON4' : {'output':'GV9', 'index': (4)}, 
-            'BOF4' : {'output':'GV9', 'index': (4)}, 
-            'BON5' : {'output':'GV10', 'index': (5)}, 
-            'BOF5' : {'output':'GV10', 'index': (5)}, 
-            'BON6' : {'output':'GV11', 'index': (6)}, 
-            'BOF6' : {'output':'GV11', 'index': (6)},
-            } 
-        
+            'BON1': {'output': 'GV6', 'index': (1)},
+            'BOF1': {'output': 'GV6', 'index': (1)},
+            'BON2': {'output': 'GV7', 'index': (2)},
+            'BOF2': {'output': 'GV7', 'index': (2)},
+            'BON3': {'output': 'GV8', 'index': (3)},
+            'BOF3': {'output': 'GV8', 'index': (3)},
+            'BON4': {'output': 'GV9', 'index': (4)},
+            'BOF4': {'output': 'GV9', 'index': (4)},
+            'BON5': {'output': 'GV10', 'index': (5)},
+            'BOF5': {'output': 'GV10', 'index': (5)},
+            'BON6': {'output': 'GV11', 'index': (6)},
+            'BOF6': {'output': 'GV11', 'index': (6)},
+        }
+
     # ON OFF Control Commands
     # All Zones
     def cmdOn(self, command):
@@ -134,7 +138,7 @@ class IrrigationNode(udi_interface.Node):
         index = self.mapping[command['cmd']]['index']
         if self.bc.binaryOutput(index) != 1:
             self.bc.binaryOutput(index, 1)
-            self.setDriver(output, 1) 
+            self.setDriver(output, 1)
             LOGGER.info('Zone {} On'.format(index))
 
     def cmdOff(self, command):
@@ -152,25 +156,25 @@ class IrrigationNode(udi_interface.Node):
             self.start()
             LOGGER.debug('shortPoll (node)')
 
-    def query(self,command=None):
+    def query(self, command=None):
         self.start()
         LOGGER.info(self.bc)
 
     drivers = [
         {'driver': 'ST', 'value': 0, 'uom': 2},
-        {'driver': 'GV0', 'value': 1, 'uom': 17}, # OSA Temp
-        {'driver': 'GV1', 'value': 1, 'uom': 56}, # Spare
-        {'driver': 'GV2', 'value': 1, 'uom': 56}, # Spare
-        {'driver': 'GV3', 'value': 1, 'uom': 56}, # Spare
-        {'driver': 'GV4', 'value': 1, 'uom': 56}, # Spare
-        {'driver': 'GV5', 'value': 1, 'uom': 25}, # Soil Moisture
+        {'driver': 'GV0', 'value': 1, 'uom': 17},  # OSA Temp
+        {'driver': 'GV1', 'value': 1, 'uom': 56},  # Spare
+        {'driver': 'GV2', 'value': 1, 'uom': 56},  # Spare
+        {'driver': 'GV3', 'value': 1, 'uom': 56},  # Spare
+        {'driver': 'GV4', 'value': 1, 'uom': 56},  # Spare
+        {'driver': 'GV5', 'value': 1, 'uom': 25},  # Soil Moisture
         {'driver': 'GV6', 'value': 0, 'uom': 80},  # Zone 1 Output
         {'driver': 'GV7', 'value': 0, 'uom': 80},  # Zone 2 Output
         {'driver': 'GV8', 'value': 0, 'uom': 80},  # Zone 3 Output
         {'driver': 'GV9', 'value': 0, 'uom': 80},  # Zone 4 Output
-        {'driver': 'GV10', 'value': 0, 'uom': 80}, # Zone 5 Output
-        {'driver': 'GV11', 'value': 0, 'uom': 80}, # Zone 6 Output
-        ]
+        {'driver': 'GV10', 'value': 0, 'uom': 80},  # Zone 5 Output
+        {'driver': 'GV11', 'value': 0, 'uom': 80},  # Zone 6 Output
+    ]
 
     id = 'zone'
 
@@ -188,5 +192,4 @@ class IrrigationNode(udi_interface.Node):
         'BON6': cmdOn,
         'BOF6': cmdOff,
         'PING': query
-                }
-    
+    }
